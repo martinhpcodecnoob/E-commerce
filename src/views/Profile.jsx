429d372c99131abe13d0bb2b2a3b4069648a97cb
JorgeTTor <jorge.ttor@gmail.com>
2021-12-16 10:55:01 -0600

    tarjeta de datos perfil

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -2,31 +2,52 @@ import React from "react";
 import withProtection from "../utils/withProtection";
 import { useUserContext } from "../context/userContext";
 
-import { Container, Card, ListGroup } from "react-bootstrap";
-
+import { Container, ListGroup, Row, Col } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Profile = () => {
   const context = useUserContext();
   console.log(context);
   return (
-    <div className="container">
-      <h1 style={{ color: "purple" }}>Mi perfil</h1>
-      <p>
-        !Hola, {context.usuarioActual.user.first_name} ! Aquí puedes ver la
-        información de tu cuenta.{" "}
-      </p>
-      {context.usuarioActual ? (
-        <>
-          <p>Nombre: {context.usuarioActual.user.first_name}</p>
-          <p>Apellido: {context.usuarioActual.user.last_name}</p>
-          <p>Fecha de nacimiento: {context.usuarioActual.user.birth_date}</p>
-          <p>Correo electrónico: {context.usuarioActual.user.email}</p>
-        </>
-      ) : (
-        <p>No puedes acceder a esta info</p>
-      )}
-    </div>
+    <Container className="container-info">
+      <div className="row list-group-info">
+        <div className="">
+          <Row className="pd-logo">
+            <Col xs={6} md={4}>
+              <Image src="personal-data.png" false rounded />
+            </Col>
+            <Col>
+              <h1 className="my-auto-perfil">Mi perfil</h1>
+            </Col>
+          </Row>
+          {context.usuarioActual ? (
+            <>
+              <ListGroup
+                variant="flush"
+                className="list-group list-group-accent"
+              >
+                <ListGroup.Item className="list-group-item list-group-item-accent-light">
+                  Nombre: {context.usuarioActual.user.first_name}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Apellido: {context.usuarioActual.user.last_name}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Fecha de nacimiento: {context.usuarioActual.user.birth_date}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Correo electrónico: {context.usuarioActual.user.email}
+                </ListGroup.Item>
+              </ListGroup>
+            </>
+          ) : (
+            <p>No puedes acceder a esta info</p>
+          )}
+        </div>
+      </div>
+    </Container>
   );
 };
+
 export default withProtection(Profile);
