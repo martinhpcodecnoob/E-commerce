6aeff630d7206a96cb9b92be225bcaff14a79017
Ednaly <yudith.cuadros@gmail.com>
2021-12-15 21:39:05 -0500

    profile

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           </label>
           {/* <!-- NAVIGATION MENUS --> */}
           <div className="menu">
-            <li>
+            <li id="search">
               <Container>
             <Form className="d-flex">
         <FormControl
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import withProtection from "../utils/withProtection";
 import { useUserContext } from "../context/userContext";
+import {ListGroup,ListGroupItem,Stack} from 'react-bootstrap';
 const Profile = () => {
   const context = useUserContext();
   console.log(context);
   return (
     <div>
-      <h1>Mi perfil</h1>
+      <h1 class="text-center">Mi perfil</h1>
       {context.usuarioActual ? (
         <>
-          <p>Nombre: {context.usuarioActual.user.first_name}</p>
-          <p>Apellido: {context.usuarioActual.user.last_name}</p>
-          <p>Apellido: {context.usuarioActual.user.birth_date}</p>
-          <p>Apellido: {context.usuarioActual.user.email}</p>
+        <Stack gap={4} className="col-md-5 mx-auto">
+        <ListGroup>
+          <ListGroup.Item variant="info">Nombre: {context.usuarioActual.user.first_name}</ListGroup.Item>
+          <ListGroup.Item variant="info">Apellido: {context.usuarioActual.user.last_name}</ListGroup.Item>
+          <ListGroup.Item variant="info">Fecha: {context.usuarioActual.user.birth_date}</ListGroup.Item>
+          <ListGroup.Item variant="info">Correo: {context.usuarioActual.user.email}</ListGroup.Item>
+        </ListGroup>
+        </Stack>
         </>
       ) : (
         <p>No puedes acceder a esta info</p>
