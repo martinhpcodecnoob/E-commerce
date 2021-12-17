90c80791e100e0291916f7d993335e72b4084818
Ednaly <yudith.cuadros@gmail.com>
2021-12-17 17:15:37 -0500

    home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,26 +5,30 @@ export default function Home() {
   return (
     <div>
   <Container className="container-home">
-  <Row>
-  <Col md="auto">
-      <h2>Compra online</h2>
-      <h3 >Los mejores productos a los mejores precios </h3>
-      <p>Encuentra accesorios, ropas, articulos para el hogar, tecnologia, etc.</p>
-      <Button variant="primary"  active>
-    Ver productos
-  </Button>
-  </Col>
-  <Col md="auto">
-      <Figure>
-  <Figure.Image
-    width={400}
-    height={400}
-    alt="400x400"
-    src="https://www.pngplay.com/wp-content/uploads/6/E-Commerce-Shopping-PNG-HD-Quality.png"
-  />
-</Figure>
-</Col>
-</Row>
+    <Container className="container-left">
+    <Row>
+    <Col md="auto">
+        <h2>Compra online</h2>
+        <h3 >Los mejores productos a los mejores precios </h3>
+        <p>Encuentra accesorios, ropas, articulos para el hogar, tecnologia, etc.</p>
+        <Button variant="primary"  active>
+      Ver productos
+    </Button>
+    </Col>
+    </Row>
+    </Container>
+    <Container className="container-right">
+      <Col md="auto">
+          <Figure>
+      <Figure.Image
+        width={450}
+        height={450}
+        alt="450x450"
+        src="https://www.pngplay.com/wp-content/uploads/6/E-Commerce-Shopping-PNG-HD-Quality.png"
+      />
+    </Figure>
+    </Col>
+    </Container>
 </Container>   
     </div>
   );
